b08de248530f29eb748876267a847a52479194e3
DJ Enriquez <denrie.enriquezjr@gmail.com>
2017-05-18 16:48:05 -0700

    Add ECS agent tag + correct cluster name

--- type/application/provision.sh
+++ type/application/provision.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+export ECS_AGENT_TAG=v1.14.1
+
 # Download all scripts and run in parallel
 aws s3 cp s3://immutable-infrastructure/${VERSION}/type/${TYPE}/provision /etc/provision/ --region us-west-2 --recursive
 
