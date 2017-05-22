bcf2572cf89e99458cf88bf66708a5d92d6d7f4f
DJ Enriquez <denrie.enriquezjr@gmail.com>
2017-05-21 17:39:26 -0700

    Fix spelling

--- type/application/provision/ecs.sh
+++ type/application/provision/ecs.sh
@@ -2,4 +2,4 @@
 set -e 
 
 # Download the ecs.config
-aws s3 cp s3://immuable-infrastructure/${VERSION}/tenant/${TENANT}/${TYPE}/ecs.config /etc/ecs/ecs.config --region us-west-2
+aws s3 cp s3://immutable-infrastructure/${VERSION}/tenant/${TENANT}/${TYPE}/ecs.config /etc/ecs/ecs.config --region us-west-2
